bfea760bb10ccea20742ca46433ef913259e7e5a
Ekoton22 <41167394+Ekoton22@users.noreply.github.com>
2018-07-12 22:07:57 +0300

    Add files via upload

---
+++ TrainItem.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dm_project3;
+
+/**
+ *
+ * @author ENE
+ * Calculates the distances of the train dataset values with the test set.
+ */
+public class TrainItem extends Instance{
+    
+    protected double distance;
+
+    public TrainItem(double[] values) {
+        super(values);
+    }
+
+    public TrainItem(double distance, double[] values) {
+        super(values);
+        this.distance = distance;
+    }
+
+    public double getDistance() {
+        return distance;
+    }
+
+    public void setDistance(double distance) {
+        this.distance = distance;
+    }
+
+    @Override
+    public String toString() {
+        return "TrainItem{" + "distance=" + distance + '}';
+    }
+    
+    
+}
